0c0d34ab8389c72cd7be66198ce74868cac52852
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2025-12-16 10:29:34 -0600

    npm install, env, database name updated, user table added, updated user router, added devtools, tested register/login in postman

--- database.sql
+++ database.sql
@@ -8,12 +8,27 @@ DROP TABLE IF EXISTS "user";
 -------------------------------------------------------
 --------------------------------------------------
 -- TABLE SCHEMAS:
+-- Default
+-- CREATE TABLE "user" (
+--   "id" SERIAL PRIMARY KEY,
+--   "username" VARCHAR (80) UNIQUE NOT NULL,
+--   "password" VARCHAR (1000) NOT NULL,
+--   "inserted_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+--   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
+-- );
+
+-- Proposed user table 
 CREATE TABLE "user" (
-  "id" SERIAL PRIMARY KEY,
-  "username" VARCHAR (80) UNIQUE NOT NULL,
-  "password" VARCHAR (1000) NOT NULL,
-  "inserted_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR(100) NOT NULL UNIQUE,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "password" VARCHAR(255) NOT NULL,
+    "first_name" VARCHAR(100) NOT NULL,
+    "last_name" VARCHAR(100) NOT NULL,
+    "role" VARCHAR(50) NOT NULL,
+    "department" VARCHAR(50),
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 
