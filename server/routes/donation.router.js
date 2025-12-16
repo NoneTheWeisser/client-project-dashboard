b77b4c3fbb13b25d2aeb3a097a0b2c5b776c4753
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2025-12-16 14:34:58 -0600

    testing out new repo

---
+++ server/routes/donation.router.js
@@ -0,0 +1,10 @@
+//  GET /api/donors
+//  POST /api/donors
+//  PUT /api/donors/:id
+//  DELETE /api/donors/:id
+
+
+//  GET /api/donations
+//  POST /api/donations
+//  PUT /api/donations/:id
+//  DELETE /api/donations/:id
